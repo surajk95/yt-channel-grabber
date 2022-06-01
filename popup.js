8634b73c9a39dfc41bb764486b104f1aa4f45758
surajk95 <surajkukati95@gmail.com>
2022-06-01 22:05:42 +0530

    completed initial version

--- popup.js
+++ popup.js
@@ -5,11 +5,9 @@ const errorContainer = document.querySelector("#error")
 
 userInput.addEventListener('input', (event) => {
     //fetch thumbnail
-    console.log(`zzz`, event)
-    const imageContainers = document.querySelectorAll(".imageContainer")
-    imageContainers.forEach(i => {i.remove()})
+    const imageParents = document.querySelectorAll(".imageParent")
+    imageParents.forEach(i => {i.remove()})
     const result = fetchImage(event.target.value.trim())
-    console.log(`zzzresult`, result)
     if(result.error) {
         errorContainer.innerHTML = result.error
         errorContainer.style.display = 'block'
@@ -18,39 +16,65 @@ userInput.addEventListener('input', (event) => {
     else {
         errorContainer.style.display = 'none'
         resultContainer.style.display = 'flex'
-        let imgContainer = null
+        let imgContainer = null, imgParent = null, imgLink = null, imgDesc = null
         if(result.maxres) {
+            imgParent = document.createElement('div')
+            imgParent.classList.add("imageParent")
+            imgDesc = document.createElement('div')
+            imgDesc.innerHTML = 'highest quality'
+            imgParent.appendChild(imgDesc)
+            imgLink = document.createElement('a')
+            imgLink.href = result.maxres
+            imgLink.setAttribute('target', '_blank')
             imgContainer = document.createElement("img")
             imgContainer.src = result.maxres
             imgContainer.classList.add("imageContainer")
-            resultContainer.appendChild(imgContainer)
+            imgLink.appendChild(imgContainer)
+            imgParent.appendChild(imgLink)
+            resultContainer.appendChild(imgParent)
         }
         if(result.hq) {
+            imgParent = document.createElement('div')
+            imgParent.classList.add("imageParent")
+            imgDesc = document.createElement('div')
+            imgDesc.innerHTML = 'medium quality'
+            imgParent.appendChild(imgDesc)
+            imgLink = document.createElement('a')
+            imgLink.href = result.hq
+            imgLink.setAttribute('target', '_blank')
             imgContainer = document.createElement("img")
             imgContainer.src = result.hq
             imgContainer.classList.add("imageContainer")
-            resultContainer.appendChild(imgContainer)
+            imgLink.appendChild(imgContainer)
+            imgParent.appendChild(imgLink)
+            resultContainer.appendChild(imgParent)
         }
         if(result.mq) {
+            imgParent = document.createElement('div')
+            imgParent.classList.add("imageParent")
+            imgDesc = document.createElement('div')
+            imgDesc.innerHTML = 'lowest quality'
+            imgParent.appendChild(imgDesc)
+            imgLink = document.createElement('a')
+            imgLink.href = result.mq
+            imgLink.setAttribute('target', '_blank')
             imgContainer = document.createElement("img")
             imgContainer.src = result.mq
             imgContainer.classList.add("imageContainer")
-            resultContainer.appendChild(imgContainer)
+            imgLink.appendChild(imgContainer)
+            imgParent.appendChild(imgLink)
+            resultContainer.appendChild(imgParent)
         }
     }
-    //resultContainer.innerHTML = event.target.value
 })
 
 const fetchImage = (url) => {
     try {
         let aaa = /(.*)(com|be)\/(watch\?v=)?(.{11})[?|&]?.*/g;
         let imgUrl = aaa.exec(url)[4];
-        //console.log(`${imgUrl}`);
-        console.log("Fetching Image for "+url);
         let newmq="https://i.ytimg.com/vi/"+imgUrl+"/mqdefault.jpg";
         let newhq="https://i.ytimg.com/vi/"+imgUrl+"/hqdefault.jpg";
         let newmaxres="https://i.ytimg.com/vi/"+imgUrl+"/maxresdefault.jpg";
-        //console.log(newImgUrl);
         return({
             mq: newmq,
             hq: newhq,
@@ -58,7 +82,6 @@ const fetchImage = (url) => {
         })
     }
     catch {
-    //console.log(`error`);
     return ({error: `Link is not valid, please try with a different one.`})
     }
   }
