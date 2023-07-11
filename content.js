5b2b3627c1fe72f06e248af3ee4bd46a84fe63ca
suraj <surajkukati95@gmail.com>
2023-07-11 20:10:34 +0530

    option to change timeout

--- content.js
+++ content.js
@@ -1,18 +1,21 @@
 let timeout = 3000, timeoutId = null
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(request.message==='get_url') {
-            sendResponse({message: window.location.href})
+    function (request, sender, sendResponse) {
+        if (request.message === 'get_url') {
+            sendResponse({ message: window.location.href })
         }
-        else if(request.message==='scroll_to_bottom') {
-            timeout = request.timeout
-            sendResponse({message: 'scrolling'})
+        else if (request.message === 'scroll_to_bottom') {
+            sendResponse({ message: 'scrolling' })
             scrollToBottom(0)
         }
-        else if(request.message==='stop_capture') {
+        else if (request.message === 'stop_capture') {
             stopScrolling()
             grabData()
+        }
+        else if (request.message === 'update_timeout') {
+            timeout = request.data
+            sendResponse(`Updated timeout to ${timeout}`)
         }
         return true
     }
@@ -22,12 +25,12 @@ function scrollToBottom(oldHeight) {
     window.scrollTo(0, document.querySelector("#content").scrollHeight)
     const newHeight = document.querySelector("#content").scrollHeight
 
-    if(newHeight===oldHeight) {
+    if (newHeight === oldHeight) {
         //send message for next step
         sendMessage(`scroll complete`)
     }
     else {
-        timeoutId = setTimeout(()=>scrollToBottom(newHeight), timeout)
+        timeoutId = setTimeout(() => scrollToBottom(newHeight), timeout)
     }
 }
 
@@ -42,14 +45,14 @@ function grabData() {
 
 function processPage() {
     const data = Array.from(document.querySelectorAll('#video-title-link'))
-    .filter(i => i.getAttribute('aria-label'))
-    .map(i => ({
-        link: i.href.split('&')[0],
-        title: i.getAttribute('aria-label').match(/(.+)\sby/)[1],
-        date: i.getAttribute('aria-label').match(/(\d+)\s(\w+)\sago/).slice(1,3),
-        duration: i.getAttribute('aria-label').match(/ago\s(.+)\s[0-9.,]+\sviews/)[1],
-        views: parseInt(i.getAttribute('aria-label').match(/([0-9.,]+)\sviews/)[1].replace(',',''))
-    }))
+        .filter(i => i.getAttribute('aria-label'))
+        .map(i => ({
+            link: i.href.split('&')[0],
+            title: i.getAttribute('aria-label').match(/(.+)\sby/)[1],
+            date: i.getAttribute('aria-label').match(/(\d+)\s(\w+)\sago/).slice(1, 3),
+            duration: i.getAttribute('aria-label').match(/ago\s(.+)\s[0-9.,]+\sviews/)[1],
+            views: parseInt(i.getAttribute('aria-label').match(/([0-9.,]+)\sviews/)[1].replace(',', ''))
+        }))
     const channel_name = document.querySelector('#meta').querySelector('#text').innerText
     const channel_handle = document.querySelector('#meta').querySelector('#channel-handle').innerText
     const subscriber_count = parseInt(document.querySelector('#meta').querySelector('#subscriber-count').innerText)
@@ -73,5 +76,5 @@ function processPage() {
 
 
 function sendMessage(message, data) {
-    const response = chrome.runtime.sendMessage({message: message, data})
+    const response = chrome.runtime.sendMessage({ message: message, data })
 }
--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
-const TIMEOUT = 3000
+let TIMEOUT = 3000
 let capturing = false
 
 const urlPattern = /youtube\.com.+videos/
 let startButton = document.querySelector("#start")
 const resultContainer = document.querySelector("#result")
 const errorContainer = document.querySelector("#error")
+const timeoutInput = document.querySelector('#timeoutInput')
 
 window.onload = () => {
     console.log(`init...`)
@@ -12,28 +13,47 @@ window.onload = () => {
 }
 
 async function checkUrlValidity() {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true})
-    const response = await chrome.tabs.sendMessage(tab.id, {message: "get_url"})
-    const url = response.message
-    if(url.match(/https:\/\/www\.youtube.com\/.+\/videos/)) {
-        show({
-            message: `valid youtube channel url`
-        })
-        //show start button
-        document.querySelector('#start').style.display = 'initial'
-        startButton.addEventListener('click', (event) => {toggleCapture()})
-    }
-    else {
+    try {
+        const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
+        const response = await chrome.tabs.sendMessage(tab.id, { message: "get_url" })
+        const url = response.message
+        if (url.match(/https:\/\/www\.youtube.com\/.+\/videos/)) {
+            show({
+                message: `valid youtube channel url`
+            })
+            //show start button
+            document.querySelector('#start').style.display = 'initial'
+            startButton.addEventListener('click', (event) => { toggleCapture() })
+        }
+        else {
+            show({
+                message: `Not a valid url. Go to the Videos tab on a youtube channel`,
+                type: 'error'
+            })
+        }
+    } catch (e) {
         show({
             message: `Not a valid url. Go to the Videos tab on a youtube channel`,
             type: 'error'
         })
     }
+    setState()
+    addEventListeners()
+}
+
+function addEventListeners() {
+    document.querySelector('#updateTimeout').addEventListener('click', function (e) {
+        updateTimeout()
+    })
+}
+
+function setState() {
+    timeoutInput.value = TIMEOUT
 }
 
 function show(payload) {
     const el = document.querySelector('#message')
-    if(!payload.message) {
+    if (!payload.message) {
         el.innerText = ''
         return
     }
@@ -42,7 +62,7 @@ function show(payload) {
 }
 
 function toggleCapture() {
-    if(!capturing) {
+    if (!capturing) {
         startButton.innerText = 'Stop'
         startCapture()
     }
@@ -56,10 +76,9 @@ async function startCapture() {
     show({
         message: 'Scrolling down...', type: 'success'
     })
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    const response = await chrome.tabs.sendMessage(tab.id, {message: "scroll_to_bottom", timeout: TIMEOUT});
-    // do something with response here, not outside the function
-    if(!response) {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    const response = await chrome.tabs.sendMessage(tab.id, { message: "scroll_to_bottom", timeout: TIMEOUT });
+    if (!response) {
         //throw error about adblock possibly interfering
     }
 }
@@ -67,29 +86,44 @@ async function stopCapture() {
     show({
         message: 'Capture stopped', type: 'success'
     })
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    const response = await chrome.tabs.sendMessage(tab.id, {message: "stop_capture", timeout: TIMEOUT});
-    // do something with response here, not outside the function
-    if(!response) {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    const response = await chrome.tabs.sendMessage(tab.id, { message: "stop_capture", timeout: TIMEOUT });
+    if (!response) {
         //throw error about adblock possibly interfering
     }
 }
 
+async function updateTimeout() {
+    const val = timeoutInput.value
+    TIMEOUT = val
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    const response = await chrome.tabs.sendMessage(tab.id, { message: "update_timeout", data: val });
+    if (!response) {
+        //throw error about adblock possibly interfering
+    }
+    else {
+        show({
+            message: response, type: 'success'
+        })
+    }
+}
+
+//listeners
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
         // console.log(`new message:`, request, sender.tab ?
         //           "from a content script:" + sender.tab.url :
         //           "from the extension");
-        if(request.message==='scroll complete') {
+        if (request.message === 'scroll complete') {
             toggleCapture()
         }
-        else if(request.message==='channel_data') {
+        else if (request.message === 'channel_data') {
             show({
                 message: 'Data copied to clipboard', type: 'success'
             })
             navigator.clipboard.writeText(JSON.stringify(request.data))
         }
         if (request.message === "hello")
-            sendResponse({farewell: "goodbye"});
+            sendResponse({ farewell: "goodbye" });
     }
 )
